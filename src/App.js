a3edcb22467e6261f08bd05b710977b408ad2627
Sandeep Chaudhary <sandee.chaudhary367@gmail.com>
2021-05-03 13:04:23 +0530

    checkout pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx';
-import Header from './components/header/header.component.jsx';
-import { Switch, Route,Redirect } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Women from './pages/women/women.component.jsx';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import Contact from './pages/contact/contact.comonent.jsx'
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+import Contact from "./pages/contact/contact.comonent.jsx"
+
+import Header from './components/header/header.component';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
-  
-  unsubscribeFromAuth=null;
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
-       
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
@@ -47,6 +52,7 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
           <Route exact path='/contact' component={Contact} />
+          <Route exact path='/checkout' component={CheckoutPage} />
          {/* <Route exact path='/shop/:number' component={Women} />*/}
           <Route
             exact
@@ -66,8 +72,8 @@ class App extends React.Component {
   }
   }
 
-  const mapStateToProps = ({ user }) => ({
-    currentUser: user.currentUser
+  const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
   });
   
   const mapDispatchToProps = dispatch => ({
